a5a0d7bf6e8591c884a158684216cc017f82c029
ekaterina <mail.mail@com>
2025-12-10 18:48:54 +0500

    Добавьте файлы проекта.

---
+++ Calculator.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main() {
+	int a, b, sum = 0;
+	std::cout << "write a and b to sum them" << std::endl;
+	std::cin >> a >> b;
+
+	sum = a + b;
+	std::cout << "Your sum is: " << sum << std::endl;
+	
+	return 0;
+}
